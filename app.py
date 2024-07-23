e17700a2cafa6b59b246dfb9f27a5f458fd6bd6b
bryan13-2 <pazbry.13@gmail.com>
2024-07-23 16:05:16 -0500

    version 04 B

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, json
 from flask_mysqldb import MySQL
 from config import Config
 import datetime
